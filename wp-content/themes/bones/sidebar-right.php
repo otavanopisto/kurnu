a96826a2711970d8b1a0a898d7f89c2be960e3e6
petja.laitila@otavanopisto.fi <petja.laitila@otavanopisto.fi>
2013-05-23 13:27:38 +0300

    Sticky listing to sidebar

--- wp-content/themes/bones/sidebar-right.php
+++ wp-content/themes/bones/sidebar-right.php
@@ -1,6 +1,8 @@
 				<div id="sidebar-right" class="sidebar fourcol last clearfix" role="complementary">
 
-
+          <ul class="sticky-listing">
+          <?php include 'sidebar-stickylisting.php'; ?>
+          </ul>
 
 					<?php if ( is_active_sidebar( 'sidebar-right' ) ) : ?>
 						<?php dynamic_sidebar( 'sidebar-right' ); ?>
