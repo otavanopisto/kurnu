c53566c91fd9b7259827effa2d95e247aa523e77
jjlankinen <jari.lankinen@otavanopisto.fi>
2013-05-30 11:51:53 +0300

    ** Changed flip animation speed ** Fixed wrong position for prev/next icons when in mobile view

--- wp-content/themes/bones/library/js/scripts.js
+++ wp-content/themes/bones/library/js/scripts.js
@@ -112,7 +112,7 @@ jQuery(document).ready(function(){
     jQuery('#siesta-listing-wrapper').flip({
       direction:'lr',
       color: 'white',
-      speed: 200,
+      speed: 100,
       onEnd: function(){
         $current = jQuery('.siesta-current');
         $prev = $current.prev(".siesta");
@@ -132,7 +132,7 @@ jQuery(document).ready(function(){
 	jQuery('#siesta-listing-wrapper').flip({
       direction:'rl',
       color: 'white',
-      speed: 200,
+      speed: 100,
       onEnd: function(){
         $current = jQuery('.siesta-current');
         $next = $current.next(".siesta");
