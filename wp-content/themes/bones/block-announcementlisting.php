afca047cde4eae076a20332e0302559269f27593
jjlankinen <jari.lankinen@otavanopisto.fi>
2013-05-31 09:52:04 +0300

    ** Layout changes

--- wp-content/themes/bones/block-announcementlisting.php
+++ wp-content/themes/bones/block-announcementlisting.php
@@ -1,24 +1,11 @@
 <?php
-  $today = date('Y-m-d');
-  $the_query = new WP_Query( array ( 'post_type' => 'announcement', 'posts_per_page' => 3, 'orderby' => 'meta_value', 'meta_key' => 'eventdate', 'order' => 'ASC',
-    'meta_query' => array(
-      array(
-        'key' => 'eventdate',
-        'value' => $today,
-        'type' => 'date',
-        'compare' => '>='
-      )
-    )
-  ));
+  $the_query = new WP_Query( array ( 'post_type' => 'announcement', 'posts_per_page' => 3, 'order' => 'ASC' ));
 
   // $the_query = new WP_Query( array ( 'post_type' => 'announcement'));
   // The Loop
 
   while ( $the_query->have_posts() ) :
   $the_query->the_post();
-  $custom_value = get_post_custom_values('eventdate', get_the_ID());
-  $originalDate = $custom_value[0];
-  $formattedDate = date("d.m.", strtotime($originalDate));
   echo '<div class="announcement-wrapper block bright-2">';
   echo '<div class="announcement-row">';
   echo '<div class="announcement-date">' . $formattedDate . '</div>';
--- wp-content/themes/bones/block-stickylisting.php
+++ wp-content/themes/bones/block-stickylisting.php
@@ -1,5 +1,13 @@
 <?php
-  $the_query = new WP_Query(array('post_type' => 'any', 'meta_key' => 'sticky', 'meta_value' => '1'));
+  $the_query = new WP_Query( array ( 'post_type' => 'any',  'order' => 'DESC', 
+    'meta_query' => array(
+      array(
+        'key' => 'sticky',
+        'value' => '1',
+        'compare' => '='
+      )
+    )
+  ));
 
   if ( $the_query->have_posts()) {
     echo '<div class="sticky-listing-wrapper block dark">';
@@ -9,17 +17,13 @@
   // The Loop
   while ( $the_query->have_posts() ) :
   $the_query->the_post();
-//   $custom_value = get_post_custom_values('eventdate', get_the_ID());
-//   $originalDate = $custom_value[0];
-//   $formattedDate = date("d.m.", strtotime($originalDate));
+  $custom_value = get_post_custom_values('eventdate', get_the_ID());
+  $originalDate = $custom_value[0];
+  $formattedDate = date("d.m.", strtotime($originalDate));
   echo '<div class="sticky-listing-container">';
   echo '<ul>';
-  echo '<li class="sidebar-sticky">';
-  // echo '<div class="sidebar-sticky-date">' . $formattedDate . '</div>';
-  echo '<div class="sidebar-sticky-title"><a href="' . get_permalink() . '">' . get_the_title() . '</a></div>';
-//   echo '<div class="sidebar-sticky-row">';
-//   echo '<div class="sidebar-sticky-excerpt">' . get_the_excerpt() . '</div>';
-//   echo '</div>';
+  echo '<li class="sticky">';
+  echo '<div class="sticky-title-wrapper"><a href="' . get_permalink() . '">' . get_the_title() . '</a></div>';
   echo '</li>';
   echo '</ul>';
   echo '</div>';
