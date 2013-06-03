200379be7d8d8129a6b49dfd28f8718d75e2f416
petja.laitila@otavanopisto.fi <petja.laitila@otavanopisto.fi>
2013-06-03 12:24:33 +0300

    Block article listing

--- wp-content/themes/bones/block-articlelisting.php
+++ wp-content/themes/bones/block-articlelisting.php
@@ -1,24 +1,27 @@
-<div class="article-listing-wrapper  block white">
-  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    
-    <div class="article-wrapper" id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
-    
-      <div class="article-title">
-    
-        <h1 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-        
-      </div>
-    
-      <div class="entry-content clearfix">
-        <?php if ( has_post_thumbnail() ) { the_post_thumbnail('large'); } ?>
-      
-        <?php the_excerpt(); ?>
-      </div>
-    
-    </div>
-    
-  <?php endwhile; ?>
+<?php 
+$args = array();
+$args['limit'] = 5;
+$args['post_type'] = 'post';
+$args['exclude'] = get_the_ID();
+$args['orderby'] = 'ID';
+$Q = new GetPostsQuery($args);
+$results = $Q->get_posts();
+
+
+echo '<div class="article-listing-wrapper  block white">';
+foreach ($results as $article){
+  echo '<div class="article-wrapper" id="post-'. $article['ID'] .'" role="article">';
+  echo '<div class="article-title">';
+  echo '<h1 class="h2"><a href="'. $article['permalink'] .'" rel="bookmark" title="'. $article['post_title'] .'">'. $article['post_title'] .'</a></h1>';
+  echo '</div>';
   
-  <?php endif; ?>
-  
-</div>
+  echo '<div class="entry-content clearfix">';
+  if ( has_post_thumbnail($article['ID']) ) {
+    echo get_the_post_thumbnail($article['ID'], 'large');
+  }
+  echo $article['post_excerpt'];
+  echo '</div>';
+  echo '</div>';
+}
+echo '</div>';
+?>
