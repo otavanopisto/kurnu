8e64f6a4454f6c4751afbbc057ee6c96abd846c5
jjlankinen <jari.lankinen@otavanopisto.fi>
2013-06-03 14:33:46 +0300

    ** Style changes only

--- wp-content/themes/bones/page.php
+++ wp-content/themes/bones/page.php
@@ -8,7 +8,7 @@
         
           <?php get_sidebar('left'); ?>
 
-					<div id="main" class="eightcol first clearfix" role="main">
+					<div id="main" class="sevencol clearfix" role="main">
 
 						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
@@ -17,9 +17,11 @@
 							<header class="article-header">
 
 								<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
+                <!-- 
 								<p class="byline vcard"><?php
 										printf(__('Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>.', 'bonestheme'), get_the_time('Y-m-j'), get_the_time(__('F jS, Y', 'bonestheme')), bones_get_the_author_posts_link());
 									?></p>
+                -->
 
 							</header> <!-- end article header -->
 
