c7a1768cc901ad65bcb3677fce606cda6eebe027
petja.laitila@otavanopisto.fi <petja.laitila@otavanopisto.fi>
2013-06-04 10:54:11 +0300

    Eventlisting fix

--- wp-content/themes/bones/block-eventlisting.php
+++ wp-content/themes/bones/block-eventlisting.php
@@ -1,46 +1,34 @@
 <?php
-  $today = date('Y-m-d');
-  $the_query = new WP_Query( array ( 'post_type' => 'event', 'posts_per_page' => 5, 'orderby' => 'meta_value', 'meta_key' => 'eventdate', 'order' => 'ASC', 
-    'meta_query' => array(
-      array(
-        'key' => 'eventdate',
-        'value' => $today,
-        'type' => 'date',
-        'compare' => '>='
-      )
-    )
-  ));
-
-  if ( $the_query->have_posts()) {
-    echo '<div class="event-listing-wrapper block white">';      
-    echo '<h3 class="event-column-title">Tulevat tapahtumat</h3>';
-  }
-
+  $args = array();
+  $args['post_type'] = 'event';
+  $args['orderby'] = 'ID';
+  $args['meta_key'] = 'eventdate';
+  $Q = new GetPostsQuery($args);
+  $results = $Q->get_posts();
+  
   // The Loop
-  while ( $the_query->have_posts() ) :
-  $the_query->the_post();
-  $custom_value = get_post_custom_values('eventdate', get_the_ID());
-  $originalDate = $custom_value[0];
-  $formattedDate = date("d.m.", strtotime($originalDate));
-  echo '<div class="event-listing-container">';
-  echo '<ul>';
-  echo '<li class="sidebar-event">';
-  echo '<div class="sidebar-event-row clearfix">';
-  echo '<div class="sidebar-event-date">' . $formattedDate . '</div>';
-  echo '<div class="sidebar-event-title"><a href="' . get_permalink() . '">' . get_the_title() . '</a></div>';
-  echo '</div>';
-  echo '<div class="sidebar-event-row">';
-  echo '<div class="sidebar-event-excerpt">' . get_the_excerpt() . '</div>';
-  echo '</div>';
-  echo '</li>';
-  echo '</ul>';
-  echo '</div>';
-  endwhile;
+  $events = array();
+  foreach ($results as $event){
+    construct_events($event, $events);    
+  }
+  ksort($events);
 
-  if ( $the_query->have_posts()) {
+  if(!empty($events)) echo '<div id="event-listing-wrapper" class="block white"><h3 class="event-column-title">Tulevat tapahtumat</h3>';
+  foreach ($events as $event_date => $single_event){
+    $formatted_date = date('d.m.', $event_date);
+    echo '<div class="event-listing-container">';
+    echo '<ul>';
+    echo '<li class="sidebar-event">';
+    echo '<div class="sidebar-event-row clearfix">';
+    echo '<div class="sidebar-event-date">' . $formatted_date . '</div>';
+    echo '<div class="sidebar-event-title"><a href="' . $single_event['permalink'] . '">' . $single_event['post_title'] . '</a></div>';
+    echo '</div>';
+    echo '<div class="sidebar-event-row">';
+    echo '<div class="sidebar-event-excerpt">' . $single_event['post_excerpt']. '</div>';
+    echo '</div>';
+    echo '</li>';
+    echo '</ul>';
     echo '</div>';
   }
-
-  wp_reset_postdata();
-
+  if(!empty($events)) echo '</div>';
 ?>
