124673961386d6afa183a8480ce1d1c7142c90bd
jjlankinen <jari.lankinen@otavanopisto.fi>
2013-06-05 10:38:32 +0300

    ** Few additional style fixes

--- wp-content/themes/bones/single-event.php
+++ wp-content/themes/bones/single-event.php
@@ -8,27 +8,26 @@
 			<div id="main" class="sevencol first clearfix" role="main">
 
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        
+          <?php 
+            $dates = (array) json_decode(get_post_meta(get_the_ID(), 'eventdate', true), true ); 
+            foreach($dates as $date){
+              $date_string .= '<div class="event-date">'. date('d.m.', strtotime($date)) .'</div>';
+            }
+          ?>        
 				<article class="block white" id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
 					<header class="article-header">
 
 						<h1 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h1>
-						<p class="byline vcard"><?php
+						
+            <p class="byline vcard"><?php
 										printf(__('Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&amp;</span> filed under %4$s.', 'bonestheme'), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), bones_get_the_author_posts_link(), get_the_category_list(', '));
 									?></p>
-                  
-            <?php 
-              $dates = (array) json_decode(get_post_meta(get_the_ID(), 'eventdate', true), true ); 
-              foreach($dates as $date){
-                $date_string .= '<div class="event-date">'. date('d.m.', strtotime($date)) .'</div>';
-              }
-            ?>
-            
+
             <div class="event-dates">
               <?php echo $date_string; ?>
             </div>
-              
+                          
             <p class="comment-count" title="Kommentit">
               <?php
                 $comments_count = wp_count_comments(get_the_ID());
