616b6e959ac22ef2a18fb2af0e4b458485ec4d5f
petja.laitila@otavanopisto.fi <petja.laitila@otavanopisto.fi>
2013-08-20 10:46:15 +0300

    AARGH

--- wp-content/themes/bones/functions.php
+++ wp-content/themes/bones/functions.php
@@ -43,7 +43,7 @@ require_once('library/bones.php'); // if you remove this, bones will break
 4. library/translation/translation.php
 	- adding support for other languages
 */
-// require_once('library/translation/translation.php'); // this comes turned off by default
+require_once('library/translation/translation.php'); // this comes turned off by default
 
 /************* THUMBNAIL SIZE OPTIONS *************/
 
