dd788b0da196655480d2d5f04ffa3ef4d65562ca
petja.laitila@otavanopisto.fi <petja.laitila@otavanopisto.fi>
2013-08-20 13:41:49 +0300

    df

--- wp-content/themes/bones/comments.php
+++ wp-content/themes/bones/comments.php
@@ -20,7 +20,7 @@ The comments page for Bones
 
 
 <?php if ( have_comments() ) : ?>
-	<h3 id="comments" class="h2"><?php comments_number(__('<span>No</span> Responses', 'bonestheme'), __('<span>One</span> Response', 'bonestheme'), _n('<span>%</span> Response', '<span>%</span> Responses', get_comments_number(),'bonestheme') );?> - &#8220;<?php the_title(); ?>&#8221;</h3>
+	<h3 id="comments" class="h2"><?php comments_number(__('<span>No</span> Responses', 'bonestheme'), __('<span>One</span> Response', 'bonestheme'), __('<span>%</span> Responses', 'bonestheme') );?> - &#8220;<?php the_title(); ?>&#8221;</h3>
 	<nav id="comment-nav">
 		<ul class="clearfix">
 				<li><?php previous_comments_link() ?></li>
