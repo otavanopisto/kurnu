489be8a516a4e7cb75064110722f936998975b42
jjlankinen <jari.lankinen@otavanopisto.fi>
2013-08-26 12:47:09 +0300

    *

--- wp-content/themes/bones/category.php
+++ wp-content/themes/bones/category.php
@@ -6,7 +6,7 @@
         
           <?php the_breadcrumb(); ?>
 
-					<div id="main" class="sevencol first clearfix" role="main">
+					<div id="main" class="tencol first clearfix" role="main">
 
 						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
@@ -14,7 +14,7 @@
 
 							<header class="article-header">
 
-								<h1 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h1>
+								<h1 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 								<p class="byline vcard"><?php
 										printf(__('Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&amp;</span> filed under %4$s.', 'bonestheme'), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), bones_get_the_author_posts_link(), get_the_category_list(', '));
 									?></p>
